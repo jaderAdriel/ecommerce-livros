227a4f9c1a7a077e48fd7c836f2dad851e85ca33
jaderAdriel <jaderadriel7@gmail.com>
2025-07-09 22:43:08 -0300

    correção de digitação errada

--- stored_procedure.sql
+++ stored_procedure.sql
@@ -24,7 +24,7 @@ BEGIN
     SELECT
 	p_data_inicio,
 	p_data_fim,
-        p_cat.nome,
+        cat.nome,
         SUM(ip.quantidade),
         SUM(ip.quantidade * ip.preco_unitario)
     FROM pedidos p
@@ -187,7 +187,7 @@ BEGIN
 
     BEGIN
         INSERT INTO Livros(titulo, autor_id, isbn, editora_id, preco, quantidade_estoque, categoria_id)
-        VALUES (p_titulo, p_autor_id, p_isbn, editora_id, p_preco, p_quantidade_estoque, v_categoria_id);
+        VALUES (p_titulo, p_autor_id, p_isbn, v_editora_id, p_preco, p_quantidade_estoque, v_categoria_id);
 
         RAISE NOTICE 'LIVRO INSERIDO COM SUCESSO';
     EXCEPTION
